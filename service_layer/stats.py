9b8c4d5a49c6ea1de9ec272af59db5d5e884231d
dimitri <dimitri.wessels@gmail.com>
2024-04-30 00:44:08 +0200

    initial

--- service_layer/stats.py
+++ service_layer/stats.py
@@ -14,3 +14,54 @@ def get_stats(repo: AbstractRepository, max_days=14) -> List[domain.DeckStat]:
 
     # display stats
     return rh.fetch_stats()
+
+
+HTML_STYLE_HEADER = """<style>
+.dimiTable {
+    width: 70%;
+    border: 2px solid black;
+}
+.dimiTable th {
+  border: 2px solid black;
+  background-color: #DDD;
+}
+</style>
+"""
+
+
+def _table(data: str):
+    return '<table class="dimiTable">\n' + data + '\n</table>'
+
+
+def _body(data: str):
+    return '<tbody>\n' + data + '\n</tbody>'
+
+
+def _head():
+    return """<thead>
+  <tr>
+    <th>#</th>
+    <th>Deck</th>
+    <th>Play Rate%</th>
+    <th>Win Rate%</th>
+    <th>[Lower, Upper]</th>
+    <th># Matches</th>
+  </tr>
+</thead>
+"""
+
+
+def _row(n, deck: domain.DeckStat) -> str:
+    return f'''  <tr>
+    <td>{n}</td>
+    <td>{deck.name}</td>
+    <td>{deck.play_rate}</td>
+    <td>{deck.win_rate.mean}</td>
+    <td>[{deck.win_rate.lower_bound}%, {deck.win_rate.upper_bound}%]</td>
+    <td>{deck.total_matches}</td>
+  </tr>'''
+
+
+def create_html_table(stats: List[domain.DeckStat]) -> str:
+    rows = "\n".join([_row(i+1, stat) for i, stat in enumerate(stats)])
+    return HTML_STYLE_HEADER+_table(_head()+_body(rows))
--- tests/unit/test_stats.py
+++ tests/unit/test_stats.py
@@ -1,4 +1,7 @@
+import domain
+import service_layer
 from domain.stats import ResultHandler
+from service_layer import stats
 
 
 def test_win_rate_calculations(izzet_phoenix_result, rakdos_vampires_result):
@@ -13,3 +16,63 @@ def test_play_rate_calculations(izzet_phoenix_result, rakdos_vampires_result):
     rh = ResultHandler([izzet_phoenix_result, rakdos_vampires_result])
     assert rh.calculate_play_rate(izzet_phoenix_result.deck_name) == 50.0
     assert rh.calculate_play_rate(rakdos_vampires_result.deck_name) == 50.0
+
+
+def test_html_table():
+    izzet_phoenix = domain.DeckStat(
+        name=domain.DeckName('Izzet Phoenix'),
+        win_rate=domain.WinRate(mean=47.94, lower_bound=44.54, upper_bound=51.35),
+        play_rate=9.1,
+        total_matches=800
+    )
+    spirits = domain.DeckStat(
+        name=domain.DeckName('Spirits'),
+        win_rate=domain.WinRate(mean=57.6, lower_bound=50.95, upper_bound=63.99),
+        play_rate=2.69,
+        total_matches=80
+    )
+    result = service_layer.stats.create_html_table([izzet_phoenix, spirits])
+    expected = """
+<style>
+.dimiTable {
+    width: 70%;
+    border: 2px solid black;
+}
+.dimiTable th {
+  border: 2px solid black;
+  background-color: #DDD;
+}
+</style>
+<table class="dimiTable">
+<thead>
+  <tr>
+    <th>#</th>
+    <th>Deck</th>
+    <th>Play Rate%</th>
+    <th>Win Rate%</th>
+    <th>[Lower, Upper]</th>
+    <th># Matches</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>1</td>
+    <td>Izzet Phoenix</td>
+    <td>9.1</td>
+    <td>47.94</td>
+    <td>[44.54%, 51.35%]</td>
+    <td>800</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Spirits</td>
+    <td>2.69</td>
+    <td>57.6</td>
+    <td>[50.95%, 63.99%]</td>
+    <td>80</td>
+  </tr>
+</tbody>
+</table>
+    """.strip()
+
+    assert result == expected
